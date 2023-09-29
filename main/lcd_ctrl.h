90e05bf8aa1ad234762515cf0d24b27586180893
Layton <caolei6767@gmail.com>
2023-09-29 20:55:51 +0800

    add lcd ctrl

---
+++ main/lcd_ctrl.h
@@ -0,0 +1,5 @@
+#ifndef LCD_CTRL_H
+#define LCD_CTRL_H
+
+void init_lcd();
+#endif
--- main/main.c
+++ main/main.c
@@ -1,7 +1,9 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "lcd_ctrl.h"
 #include "led_ctrl.h"
+#include "misc/lv_timer.h"
 #include "sdkconfig.h"
 
 #include <stdio.h>
@@ -10,13 +12,14 @@ static const char* TAG = "Main";
 
 void app_main(void)
 {
-
     ESP_LOGI(TAG, "My ESP32-S3 Ctrl Test!");
     /* Configure the peripheral according to the LED type */
     init_led();
 
+    init_lcd();
     while (1) {
-        blink_led();
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+        vTaskDelay(pdMS_TO_TICKS(10));
+        // blink_led();
+        lv_timer_handler();
     }
 }
