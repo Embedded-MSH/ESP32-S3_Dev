9b547e09644e4b1c9d4f8ba6044af1c611a38434
Layton <caolei6767@gmail.com>
2023-10-07 16:08:00 +0800

    update wifi ap mode components

--- main/main.c
+++ main/main.c
@@ -13,7 +13,7 @@ static const char* TAG = "Main";
 
 void app_main(void)
 {
-    ESP_LOGI(TAG, "My ESP32-S3 Ctrl Test!");
+    ESP_LOGI(TAG, "My ESP32-S3 init!");
 
     ESP_LOGI(TAG, "Init Led Ctrl!");
     init_led();
